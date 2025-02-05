8efeeb5e18dd2d88847915257980883cff285ed4
zeleznyt <tomas@zelezny.name>
2025-02-05 19:15:40 +0100

    update predict script for new dataloader

--- eval/predict.py
+++ eval/predict.py
@@ -10,39 +10,36 @@ from transformers import T5Config
 from model.modeling_t5 import T5ModelForSLT
 from utils.translation import postprocess_text
 import evaluate
+import yaml
+from dataset.generic_sl_dataset import SignFeatureDataset as DatasetForSLT
 
 load_dotenv()
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Run inference with a fine-tuned T5 model for SLT")
 
-    # Model and data paths
-    parser.add_argument("--model_name", type=str, default="h2s-test", help="Model name or folder inside model_dir.")
-    parser.add_argument("--dataset_type", type=str, default="yasl", choices=["how2sign", "yasl", "yasl_copy"], help="Type of the dataset.")
-    parser.add_argument("--dataset_dir", type=str, default="/path/to/data", help="Path to the dataset directory.")
-    parser.add_argument("--output_dir", default='./results',type=str)
+    # Configuration
+    parser.add_argument("--config_file", type=str, required=True, default='config.yaml')
 
-    # New data scheme
-    parser.add_argument("--annotation_file", type=str, required=True, help="Path to the annotations file.")
-    parser.add_argument("--metadata_file", type=str, required=True, help="Path to the metadata file.")
+    # Model and data paths
+    parser.add_argument("--model_name", type=str, default=None, help="Model name or folder inside model_dir.")
+    parser.add_argument("--output_dir", type=str, default=None)
 
     # Data processing
-    parser.add_argument("--skip_frames", action="store_true")
-    parser.add_argument("--max_sequence_length", type=int, default=250, help="Max number of frames for sign inputs.")
-    parser.add_argument("--max_token_length", type=int, default=128, help="Max token length for labels.")
-    parser.add_argument("--transform", type=str, default="yasl", choices=["yasl", "custom"], help="Data transform type.")
-    parser.add_argument("--modality", type=str, default="pose", choices=["pose", "sign2vec", "mae"], help="Input modality.")
+    parser.add_argument("--max_sequence_length", type=int, default=None, help="Max number of frames for sign inputs.")
+    parser.add_argument("--max_token_length", type=int, default=None, help="Max token length for labels.")
+    parser.add_argument("--skip_frames", default=None)
 
     # Generation parameters
-    parser.add_argument("--model_dir", type=str, required=True, help="Path to the directory containing the fine-tuned model and config.")
-    parser.add_argument("--batch_size", type=int, default=1, help="Batch size for inference.")
-    parser.add_argument("--pose_dim", type=int, default=208, help="Dimension of the pose embeddings.")
+    parser.add_argument("--model_dir", type=str, default=None, help="Path to the directory containing the fine-tuned model and config.")
+    parser.add_argument("--batch_size", type=int, default=None, help="Batch size for inference.")
+    # parser.add_argument("--pose_dim", type=int, default=208, help="Dimension of the pose embeddings.")
 
     # Evaluation arguments
-    parser.add_argument("--num_beams", type=int, default=5, help="Number of beams for beam search.")
-    parser.add_argument("--length_penalty", type=float, default=0.6, help="Length penalty for generation.")
-    parser.add_argument("--early_stopping", action="store_true", help="Use early stopping in generation.")
-    parser.add_argument("--no_repeat_ngram_size", type=int, default=0, help="No repeat ngram size.")
+    parser.add_argument("--num_beams", type=int, default=None, help="Number of beams for beam search.")
+    parser.add_argument("--length_penalty", type=float, default=None, help="Length penalty for generation.")
+    parser.add_argument("--early_stopping", type=bool, default=None, help="Use early stopping in generation.")
+    parser.add_argument("--no_repeat_ngram_size", type=int, default=None, help="No repeat ngram size.")
 
     # Running arguments
     parser.add_argument("--dev", action="store_true", help="Use dev mode.")
@@ -50,8 +47,42 @@ def parse_args():
     parser.add_argument("--is_normalized", action="store_true", help="If the data is normalized.")
 
     parser.add_argument("--verbose", action="store_true", help="Verbose mode.")
-    
+
     return parser.parse_args()
+
+
+def load_config(cfg_path):
+    """
+    Load config from a yaml file. 'none' and 'None' values are replaced by None value.
+    Args:
+        cfg_path: Path to config file
+    Returns:
+        config (dict): Config
+    """
+    with open(cfg_path, 'r') as file:
+        cfg = yaml.safe_load(file)
+    for param, value in cfg['EvaluationArguments'].items():
+        if value == 'none' or value == 'None':
+            cfg['EvaluationArguments'][param] = None
+
+    return cfg
+
+
+def update_config(cfg, args):
+    """
+    Update config with args passed. Default None arguments are ignored.
+    Args:
+        cfg (dict): Config
+        args (argparse.Namespace): Argument parsed from the command-line
+    Returns:
+        cfg (dict): Updated config
+    """
+    for k, v in vars(args).items():
+        if k in cfg['EvaluationArguments'] and v is not None:
+            cfg['EvaluationArguments'][k] = v
+            if os.environ.get("LOCAL_RANK", "0") == "0" and args.verbose:
+                print('Config value updated by args - {}: {}'.format(k, v))
+    return cfg
 
 def collate_fn(batch, max_sequence_length, max_token_length, pose_dim):
     return {
@@ -73,54 +104,26 @@ def collate_fn(batch, max_sequence_length, max_token_length, pose_dim):
         ]).squeeze(0).to(torch.long),
     }
 
-def load_dataset(args):
-    if args.dataset_type == 'how2sign':
-        from sign2vec.dataset.how2sign import How2SignForSLT as DatasetForSLT
-        mode = 'test' if not args.dev else 'dev'
-    elif args.dataset_type == 'yasl':
-        from sign2vec.dataset.yasl import YoutubeASLForSLT as DatasetForSLT
-        mode = 'dev' if args.dev else 'test'
-    elif args.dataset_type == 'yasl_copy':
-        from sign2vec.dataset.yasl_version_of_h2s import YoutubeASLForSLT as DatasetForSLT
-        mode = 'dev' if args.dev else 'test'
-    else:
-        raise ValueError(f"Dataset type {args.dataset_type} not supported")
-
-    dataset = DatasetForSLT(
-        h5_fpath=args.dataset_dir,
-        mode=mode,
-        transform=args.transform,
-        max_token_length=args.max_token_length,
-        max_sequence_length=args.max_sequence_length,
-        skip_frames=False,
-        tokenizer=args.model_dir,
-        max_instances=args.max_val_samples,
-        input_type=args.modality,
-        annotation_fpath=args.annotation_file,
-        metadata_fpath=args.metadata_file,
-        is_normalized=args.is_normalized,
-        verbose=args.verbose,
-    )
-    return dataset
-
-def evaluate_model(model, dataloader, tokenizer, args):
+def evaluate_model(model, dataloader, tokenizer, evaluation_config):
     model.eval()
     predictions, labels = [], []
     with torch.no_grad():
         for step, batch in enumerate(dataloader):
             batch = {k: v.to(model.base_model.device) for k, v in batch.items()}
+            if len(batch['labels'].shape) < 2:
+                batch['labels'] = batch['labels'].unsqueeze(0)
             outputs = model.generate(
                 **batch,
-                early_stopping=args.early_stopping,
-                no_repeat_ngram_size=args.no_repeat_ngram_size,
-                max_length=args.max_sequence_length,
-                num_beams=args.num_beams,
+                early_stopping=model.config.early_stopping,
+                no_repeat_ngram_size=model.config.no_repeat_ngram_size,
+                max_length=evaluation_config['max_sequence_length'],
+                num_beams=model.config.num_beams,
                 bos_token_id=tokenizer.pad_token_id,
-                length_penalty=args.length_penalty,
+                length_penalty=model.config.length_penalty,
             )
             # Replace invalid tokens with <unk>
-            if len(np.where(outputs.cpu().numpy() > len(tokenizer) - 1)[1]) > 0 and args.verbose:
-                print(f'Replacing <unk> for illegal tokens found on indexes {np.where(outputs > len(tokenizer) - 1)[1]}')
+            if len(np.where(outputs.cpu().numpy() > len(tokenizer) - 1)[1]) > 0:
+                print(f'Replacing <unk> for illegal tokens found on indexes {np.where(outputs.cpu().numpy() > len(tokenizer) - 1)[1]}')
             outputs[outputs > len(tokenizer) - 1] = tokenizer.unk_token_id
 
             decoded_preds = tokenizer.batch_decode(outputs, skip_special_tokens=True)
@@ -132,30 +135,51 @@ def evaluate_model(model, dataloader, tokenizer, args):
 
 def main():
     args = parse_args()
+    if os.environ.get("LOCAL_RANK", "0") == "0" and args.verbose:
+        print('Loading config...')
+    config = load_config(args.config_file)
+    config = update_config(config, args)
+
+    evaluation_config = config['EvaluationArguments']
+    model_config = config['ModelArguments']
+
+    # Initialize the custom model
+    t5_config = SignT5Config()
+    for param, value in model_config.items():
+        if param not in vars(t5_config):
+            print('f{param} not in SignT5Config. It may be ignored...}')
+        t5_config.__setattr__(param, value)
 
     # Load model and tokenizer
-    model = T5ModelForSLT.from_pretrained(args.model_dir)
+    model = T5ModelForSLT.from_pretrained(evaluation_config['model_dir'], config=t5_config)
     for param in model.parameters(): param.data = param.data.contiguous()
-    tokenizer = T5Tokenizer.from_pretrained(args.model_dir)
+    tokenizer = T5Tokenizer.from_pretrained(model.config.base_model_name, clean_up_tokenization_spaces=True)
 
     # Prepare dataset
-    dataset = load_dataset(args)
+    dataset = DatasetForSLT(tokenizer= tokenizer,
+                                sign_data_args=config['SignDataArguments'],
+                                split=evaluation_config['split'],
+                                skip_frames=evaluation_config['skip_frames'],
+                                max_token_length=evaluation_config['max_token_length'],
+                                max_sequence_length=evaluation_config['max_sequence_length'],
+                                max_samples=evaluation_config['max_val_samples'],
+                                )
 
     dataloader = torch.utils.data.DataLoader(
         dataset,
-        batch_size=args.batch_size,
+        batch_size=evaluation_config['batch_size'],
         collate_fn=lambda batch: collate_fn(
             batch,
-            max_sequence_length=args.max_sequence_length,
-            max_token_length=args.max_token_length,
-            pose_dim=args.pose_dim
+            max_sequence_length=evaluation_config['max_sequence_length'],
+            max_token_length=evaluation_config['max_token_length'],
+            pose_dim=config['SignModelArguments']['projectors']['pose']['dim'],
         ),
     )
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
 
-    predictions, labels = evaluate_model(model, dataloader, tokenizer, args)
+    predictions, labels = evaluate_model(model, dataloader, tokenizer, evaluation_config)
 
     # Postprocess predictions and references
     decoded_preds, decoded_labels = postprocess_text(predictions, [ref[0] for ref in labels])
@@ -193,8 +217,8 @@ def main():
     ]
     all_predictions = {'metrics': result, 'predictions': all_predictions[:100]}
 
-    os.makedirs(args.output_dir, exist_ok=True)
-    prediction_file = os.path.join(args.output_dir, "predictions.json")
+    os.makedirs(evaluation_config['output_dir'], exist_ok=True)
+    prediction_file = os.path.join(evaluation_config['output_dir'], "predictions.json")
     with open(prediction_file, "w") as f:
         json.dump(all_predictions, f, ensure_ascii=False, indent=4)
 
