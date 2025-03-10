fe8658bc901c7fd6f2bc57c2affd4cfa3e9470f4
zeleznyt <tomas@zelezny.name>
2025-03-10 21:42:24 +0100

    add individual augmentations

--- utils/augmentation_config.py
+++ utils/augmentation_config.py
@@ -25,39 +25,18 @@ light = [
         {'name': 'rotate_hand', 'angle': (-5.0, 5.0), 'rotation_center': 'wrist', 'p': 0.38},
         {'name': 'noise', 'std': 1.5, 'p': 0.5}
 ]
-individual_1 = [
-        {'name': 'rotate', 'angle': (-3.0, 3.0), 'p': 0.5}
-]
-individual_2 = [
-        {'name': 'shear', 'angle_x': (-3.0, 3.0), 'angle_y': (-3.0, 3.0), 'p': 0.38}
-]
-individual_3 = [
-        {'name': 'perspective', 'portion': (-0.08, 0.08), 'reference_size': 512, 'p': 0.25}
-]
-individual_4 = [
-        {'name': 'rotate_hand', 'angle': (-5.0, 5.0), 'rotation_center': 'shoulder', 'p': 0.38}
-]
-individual_5 = [
-        {'name': 'rotate_hand', 'angle': (-5.0, 5.0), 'rotation_center': 'elbow', 'p': 0.38}
-]
-individual_6 = [
-        {'name': 'rotate_hand', 'angle': (-5.0, 5.0), 'rotation_center': 'wrist', 'p': 0.38}
-]
-individual_7 = [
-        {'name': 'noise', 'std': 1.5, 'p': 0.5}
-]
 
 augmentations = {'none': [],
                  'light': light,
                  'medium': medium,
                  'heavy': heavy,
-                 'individual_1': individual_1,
-                 'individual_2': individual_2,
-                 'individual_3': individual_3,
-                 'individual_4': individual_4,
-                 'individual_5': individual_5,
-                 'individual_6': individual_6,
-                 'individual_7': individual_7,
+                 'individual_1': [medium[0]],
+                 'individual_2': [medium[1]],
+                 'individual_3': [medium[2]],
+                 'individual_4': [medium[3]],
+                 'individual_5': [medium[4]],
+                 'individual_6': [medium[5]],
+                 'individual_7': [medium[6]],
                  }
 
 def get_augmentations(augmentation_type: str) -> list:
