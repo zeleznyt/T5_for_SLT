86829aeeb45d2bac2149fe10f0e689d9f2ffded7
zeleznyt <tomas@zelezny.name>
2025-03-14 16:51:30 +0100

    add no rotate augmentation protocols

--- utils/augmentation_config.py
+++ utils/augmentation_config.py
@@ -25,11 +25,38 @@ light = [
         {'name': 'rotate_hand', 'angle': (-5.0, 5.0), 'rotation_center': 'wrist', 'p': 0.38},
         {'name': 'noise', 'std': 1.5, 'p': 0.5}
 ]
+heavy2 = [ # No rotate
+        {'name': 'shear', 'angle_x': (-6, 6), 'angle_y': (-6, 6), 'p': 0.75},
+        {'name': 'perspective', 'portion': (-0.15, 0.15), 'reference_size': 512, 'p': 0.5},
+        {'name': 'rotate_hand', 'angle': (-10, 10), 'rotation_center': 'shoulder', 'p': 0.75},
+        {'name': 'rotate_hand', 'angle': (-10, 10), 'rotation_center': 'elbow', 'p': 0.75},
+        {'name': 'rotate_hand', 'angle': (-10, 10), 'rotation_center': 'wrist', 'p': 0.75},
+        {'name': 'noise', 'std': 1.5, 'p': 1.0}
+]
+medium2 = [ # No rotate
+        {'name': 'shear', 'angle_x': (-4.5, 4.5), 'angle_y': (-4.5, 4.5), 'p': 0.56},
+        {'name': 'perspective', 'portion': (-0.11, 0.11), 'reference_size': 512, 'p': 0.38},
+        {'name': 'rotate_hand', 'angle': (-7.5, 7.5), 'rotation_center': 'shoulder', 'p': 0.56},
+        {'name': 'rotate_hand', 'angle': (-7.5, 7.5), 'rotation_center': 'elbow', 'p': 0.56},
+        {'name': 'rotate_hand', 'angle': (-7.5, 7.5), 'rotation_center': 'wrist', 'p': 0.56},
+        {'name': 'noise', 'std': 1.5, 'p': 0.75}
+]
+light2 = [ # No rotate
+        {'name': 'shear', 'angle_x': (-3.0, 3.0), 'angle_y': (-3.0, 3.0), 'p': 0.38},
+        {'name': 'perspective', 'portion': (-0.08, 0.08), 'reference_size': 512, 'p': 0.25},
+        {'name': 'rotate_hand', 'angle': (-5.0, 5.0), 'rotation_center': 'shoulder', 'p': 0.38},
+        {'name': 'rotate_hand', 'angle': (-5.0, 5.0), 'rotation_center': 'elbow', 'p': 0.38},
+        {'name': 'rotate_hand', 'angle': (-5.0, 5.0), 'rotation_center': 'wrist', 'p': 0.38},
+        {'name': 'noise', 'std': 1.5, 'p': 0.5}
+]
 
 augmentations = {'none': [],
                  'light': light,
                  'medium': medium,
                  'heavy': heavy,
+                 'light2': light2,
+                 'medium2': medium2,
+                 'heavy2': heavy2,
                  'individual-1': [medium[0]],
                  'individual-2': [medium[1]],
                  'individual-3': [medium[2]],
