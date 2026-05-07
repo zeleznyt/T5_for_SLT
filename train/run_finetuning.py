c7be6a444d4e80a10d3686fc4f6515d8c47f1552
zeleznyt <tomas@zelezny.name>
2026-05-07 14:45:03 +0200

    remove wer

--- train/run_finetuning.py
+++ train/run_finetuning.py
@@ -414,12 +414,6 @@ if __name__ == "__main__":
             'bleu-4': bleu4.score,
         }
 
-        wer_metric = evaluate.load("wer")
-        flat_labels = [lab[0] if isinstance(lab, (list, tuple)) else lab for lab in decoded_labels]
-        wer_score = wer_metric.compute(predictions=decoded_preds, references=flat_labels)
-        print(f"Word Error Rate: {wer_score}")
-        result["wer"] = wer_score
-
         prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
         result["gen_len"] = np.mean(prediction_lens)
         result = {k: round(v, 4) for k, v in result.items()}
